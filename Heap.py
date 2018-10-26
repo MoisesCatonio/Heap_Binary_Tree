66df7b7752625af66ddd753d22c99510104fd6cd
Moisés Catônio <moises.catonio@gmail.com>
2018-10-26 01:15:02 -0300

    adding swap procedure, own_position attribute to element class

--- Heap.py
+++ Heap.py
@@ -1,7 +1,8 @@
 class element:
-    def __init__(self, data=None, father=None, left_son=None, right_son=None):
+    def __init__(self, data=None, own_pos=None, father=None, left_son=None, right_son=None):
         self.data = data
         self.father = father
+        self.own_pos = own_pos
         self.left_son = left_son
         self.right_son = right_son
 
@@ -11,21 +12,22 @@ class heap:
 
     def determine_positions(self):
         for i in range(0, len(self.tree)):
+            self.own_pos = i
             self.tree[i].left_son = 2*i+1
             self.tree[i].right_son = 2*i+2
             if(i != 0):
                 self.tree[i].father = (i-1)/2
             else:
                 self.tree[i].father = None
-                
+
     def add_element(self, new_element):
         self.tree.append(new_element)
         self.determine_positions()
 
     def swap(self, elem1, elem2):
-        aux = elem1
-        elem1 = elem2
-        elem2 = aux
+        aux = self.tree[elem1.own_pos]
+        self.tree[elem1.own_pos] = self.tree[elem2.own_pos] 
+        self.tree[elem2.own_pos] = aux
 
     def get_dado(self, position):
         return self.tree[position].data
